08fda3fdcdffd745f67b49223dd409b20b405b08
Arthur V <vaskinel.artur@gmail.com>
2024-07-22 14:15:30 +0300

    minor update

--- src/app/service/users/users.service.ts
+++ src/app/service/users/users.service.ts
@@ -39,9 +39,6 @@ export class UsersService {
             }
           )
           this.isLoggedInSubject.next(data.token)
-        },
-        error: err => {
-          return err
         }
       }
     ))
