8b5c4be3aaf7587514bfe2af44bf70a13e52ff07
Arthur V <vaskinel.artur@gmail.com>
2024-08-02 12:50:25 +0300

    TM-45: Translation now is fetched from backend, changed how user is being set when user does login or registration

--- src/app/translate-loader.ts
+++ src/app/translate-loader.ts
@@ -1,6 +1,7 @@
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { HttpClient } from '@angular/common/http'
+import { i18nApiUrl } from './utils/constants'
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, i18nApiUrl)
 }
